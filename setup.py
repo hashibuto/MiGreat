eabeea54d74aabbf5c75375a434a5646c7deb277
hashibuto <hashibuto@noreply.github.com>
2021-10-14 11:16:26 +0900

    fixed long description for pypi

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 import os
 from setuptools import setup, find_packages
 
+# Load the README.md
+from pathlib import Path
+here = Path(__file__).parent
+long_description = (here / "README.md").read_text()
+
 # Load the requirements file in here to avoid duplication
 with open("./requirements.txt", "r") as req_file:
     requirements = [r for r in req_file.readlines() if r.strip()]
 
 setup(
     name='MiGreat',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='Philip Stefou',
     author_email='hashibuto@noreply.com',
     description='A schema isolated SQLAlchemy migrator for shared Postgres db micro services',
-    long_description="README.md",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/hashibuto/MiGreat",
     scripts=[
         "migreat/bin/migreat"
